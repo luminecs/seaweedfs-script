621c953a296a1b75a6bf1ebfb3ecfaab86d65751
luminecs <wangxuke.china@gmail.com>
2025-06-13 16:49:22 +0800

    commit

--- master01-start-9333.sh
+++ master01-start-9333.sh
@@ -2,5 +2,8 @@ nohup weed master \
 -ip=192.168.60.59 \
 -mdir=/home/lumine/seaweedfs-data/master01 \
 -defaultReplication=000 \
+-garbageThreshold=0.3 \
+-volumePreallocate \
 > /home/lumine/seaweedfs-data/master01.log 2>&1&
 
+#-volumeSizeLimitMB=8192 \
