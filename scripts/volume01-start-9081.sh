2de3ce5d455a2679e2cb4c23b1a669dc8a085c3a
luminecs <wangxuke.china@gmail.com>
2025-06-17 10:23:01 +0800

    services

--- scripts/volume01-start-9081.sh
+++ scripts/volume01-start-9081.sh
@@ -2,7 +2,7 @@ nohup weed volume \
 -ip=192.168.60.59  \
 -port=9081 \
 -dir=/home/lumine/seaweedfs-data/volume01 \
--fileSizeLimitMB=10000 \
+-fileSizeLimitMB=10240 \
 -mserver=192.168.60.59:9333 \
 -dataCenter=dc1 \
 -rack=rack1 \
